ebd83d29359f0aacbb90c032242d0909dd768801
Deschant Kounou <kounoudeschant@gmail.com>
2020-01-14 19:15:09 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Joke from './Joke';
 import Footer from './Footer';
-import './App.css';
+import About from './About';
 
 function App() {
   return (
     <div className="page__body">
       <Header />
-      <Joke />
+      <Switch>
+        <Route exact path="/" component={Joke} />
+        <Route exact path="/about" component={About} />
+      </Switch>
       <Footer />
     </div>
   );
